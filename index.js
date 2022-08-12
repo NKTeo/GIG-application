9f7bbc1263f2ee996792fe2b6333f73d69d64a7f
NKTeo <51158551+NKTeo@users.noreply.github.com>
2022-08-12 11:20:23 +0800

    chore: add ecs deployment script in github workflows

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) => res.send(
     `
-        <h1>Application successfully migrated to AWS!</h1>
+        <h1>Application successfully migrated to AWS!!!</h1>
         <p>Done by Nian Kai @ Govtech TAP 2023</p>
     `
 ))
